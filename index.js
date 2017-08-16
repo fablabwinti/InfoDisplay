61c9ebb5159f357423558eff3257bc477389f65d
Claudio Prezzi <cprezzi@comsulting.ch>
2017-08-17 01:05:11 +0200

    Basic Website with bootstrap

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+"use strict";
 var fs = require('fs');
 var readline = require('readline');
 var google = require('googleapis');
 var googleAuth = require('google-auth-library');
+var express = require('express');
+var app = express();
+var router = express.Router();
+var path = __dirname + '/views/';
+  
+var labmanagers = {};
+var workshops = {};
 
 // If modifying these scopes, delete your previously saved credentials
 // at ~/.credentials/calendar-nodejs-quickstart.json
@@ -9,6 +17,7 @@ var SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
 var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
     process.env.USERPROFILE) + '/.credentials/';
 var TOKEN_PATH = TOKEN_DIR + 'calendar-nodejs-quickstart.json';
+
 
 // Load client secrets from a local file.
 fs.readFile('client_secret.json', function processClientSecrets(err, content) {
@@ -130,7 +139,7 @@ function listEvents(auth) {
     auth: auth,
     calendarId: 'primary',
     timeMin: (new Date()).toISOString(),
-    maxResults: 15,
+    maxResults: 10,
     singleEvents: true,
     orderBy: 'startTime'
   }, function(err, response) {
@@ -139,16 +148,17 @@ function listEvents(auth) {
       return;
     }
     var events = response.items;
+    labmanagers = events;
     if (events.length == 0) {
       console.log('No upcoming events found.');
     } else {
-      console.log('Upcoming ' + events.length +' openings:');
-      for (var i = 0; i < events.length; i++) {
-        var event = events[i];
-        var start = event.start.dateTime || event.start.date;
-        var end = event.end.dateTime || event.end.date;
-        console.log('%s - %s : %s', start, end, event.summary);
-      }
+      console.log('Got ' + events.length +' upcoming LabManagers from Google Calendar.');
+//      for (var i = 0; i < events.length; i++) {
+//        var event = events[i];
+//        var start = event.start.dateTime || event.start.date;
+//        var end = event.end.dateTime || event.end.date;
+//        console.log('%s - %s : %s', start, end, event.summary);
+//      }
     }
   });
   listWorkshops(auth);
@@ -165,7 +175,7 @@ function listWorkshops(auth) {
     auth: auth,
     calendarId: '6mi8quogdni3bvnjhe787ci7i4@group.calendar.google.com',
     timeMin: (new Date()).toISOString(),
-    maxResults: 15,
+    maxResults: 8,
     singleEvents: true,
     orderBy: 'startTime'
   }, function(err, response) {
@@ -174,16 +184,72 @@ function listWorkshops(auth) {
       return;
     }
     var events = response.items;
+    workshops = events;
     if (events.length == 0) {
       console.log('No upcoming events found.');
     } else {
-      console.log('Upcoming ' + events.length +' workshops:');
-      for (var i = 0; i < events.length; i++) {
-        var event = events[i];
-        var start = event.start.dateTime || event.start.date;
-        var end = event.end.dateTime || event.end.date;
-        console.log('%s - %s : %s', start, end, event.summary);
-      }
+      console.log('Got ' + events.length +' upcoming Workshops from Google Calendar.');
+//      for (var i = 0; i < events.length; i++) {
+//        var event = events[i];
+//        var start = event.start.dateTime || event.start.date;
+//        var end = event.end.dateTime || event.end.date;
+//        console.log('%s - %s : %s', start, end, event.summary);
+//      }
     }
   });
 }
+
+
+// serve static content
+app.use(express.static('public'));
+
+app.use('/', router);
+
+router.get('/',function(req, res){
+  res.sendFile(path + 'index.html');
+});
+  
+router.get('/product',function(req, res){
+  res.sendFile(path + 'product.html');
+});
+  
+router.get('/about',function(req, res){
+  res.sendFile(path + 'about.html');
+});
+  
+app.use('*',function(req, res){
+  res.send('Error 404: Not Found!');
+});
+  
+app.listen(3000,function(){
+  console.log('Server running at Port 3000');
+});
+
+
+/*app.get('/', function (req, res) {
+    var body; 
+    body = '<h2>LabManagers</h2>';
+    body += '<ul>';
+    for (var i = 0; i < labmanagers.length; i++) {
+        var labmanager = labmanagers[i];
+        var start = labmanager.start.dateTime || labmanager.start.date;
+        var end = labmanager.end.dateTime || labmanager.end.date;
+        body += '<li>' + start + ' - ' + end + ' | ' + labmanager.summary + '</li>';
+    }
+    body += '</ul>';
+    body += '<h2>Workshops</h2>';
+    body += '<ul>';
+    for (var i = 0; i < workshops.length; i++) {
+        var workshop = workshops[i];
+        var start = workshop.start.dateTime || workshop.start.date;
+        var end = workshop.end.dateTime || workshop.end.date;
+        body += '<li>' + start + ' - ' + end + ' | ' + workshop.summary + '</li>';
+    }
+    body += '</ul>';
+    res.send(body);
+});
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
+*/
+  
